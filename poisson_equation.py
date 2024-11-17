ef0ebb0dc276edcd3a996fd83bca695df75e7ad3
OzanAkturk <e230392@metu.edu.tr>
2024-11-17 13:51:00 +0000

    poisson example (pyvista error)

--- poisson_equation.py
+++ poisson_equation.py
@@ -27,5 +27,77 @@ u = ufl.TrialFunction(V)
 v = ufl.TestFunction(V)
 
 # Defining the source term
-from dolfinx import default_scalar_type
-f = fem.Constant(domain, default_scalar_type(-6))
+# from dolfinx.fem import default_scalar_type # (doesnt work as default_scalar_type was introduced in between 0.6.0 and 0.7.0)
+f = fem.Constant(domain, -6.0)
+
+# Defining the variational problem
+a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+L = f * v * ufl.dx
+
+# Forming and solving the linear system
+from dolfinx.fem.petsc import LinearProblem
+problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+# Computing the error
+V2 = fem.FunctionSpace(domain, ("Lagrange", 2))
+uex = fem.Function(V2)
+uex.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
+
+L2_error = fem.form(ufl.inner(uh - uex, uh - uex) * ufl.dx)
+error_local = fem.assemble_scalar(L2_error)
+error_L2 = numpy.sqrt(domain.comm.allreduce(error_local, op=MPI.SUM))
+
+error_max = numpy.max(numpy.abs(uD.x.array-uh.x.array))
+# Only print the error on one process
+if domain.comm.rank == 0:
+    print(f"Error_L2 : {error_L2:.2e}")
+    print(f"Error_max : {error_max:.2e}")
+
+"""
+# Plotting the mesh using pyvista
+import pyvista
+
+from dolfinx import plot
+pyvista.start_xvfb()
+domain.topology.create_connectivity(tdim, tdim)
+topology, cell_types, geometry = plot.vtk_mesh(domain, tdim)
+grid = pyvista.UnstructuredGrid(topology, cell_types, geometry)
+
+plotter = pyvista.Plotter()
+plotter.add_mesh(grid, show_edges=True)
+plotter.view_xy()
+if not pyvista.OFF_SCREEN:
+    plotter.show()
+else:
+    figure = plotter.screenshot("fundamentals_mesh.png")
+
+# Plotting a function using pyvista
+u_topology, u_cell_types, u_geometry = plot.vtk_mesh(V)
+u_grid = pyvista.UnstructuredGrid(u_topology, u_cell_types, u_geometry)
+u_grid.point_data["u"] = uh.x.array.real
+u_grid.set_active_scalars("u")
+u_plotter = pyvista.Plotter()
+u_plotter.add_mesh(u_grid, show_edges=True)
+u_plotter.view_xy()
+if not pyvista.OFF_SCREEN:
+    u_plotter.show()
+
+warped = u_grid.warp_by_scalar()
+plotter2 = pyvista.Plotter()
+plotter2.add_mesh(warped, show_edges=True, show_scalar_bar=True)
+if not pyvista.OFF_SCREEN:
+    plotter2.show()"""
+
+
+# External post-processing
+from dolfinx import io
+from pathlib import Path
+results_folder = Path("poisson_results")
+results_folder.mkdir(exist_ok=True, parents=True)
+filename = results_folder / "fundamentals"
+with io.VTXWriter(domain.comm, filename.with_suffix(".bp"), [uh]) as vtx:
+    vtx.write(0.0)
+with io.XDMFFile(domain.comm, filename.with_suffix(".xdmf"), "w") as xdmf:
+    xdmf.write_mesh(domain)
+    xdmf.write_function(uh)
